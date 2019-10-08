24f0282e1d03d5f45752ac899e9d1fd8439f73c1
jazz4rabbit <jazz4rabbit@gmail.com>
2019-10-09 02:05:03 +0900

    :sparkles: BOJ/10926; ??! (http://noj.am/10926)

---
+++ BOJ/10926/main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+#define BS 51
+char buf[BS];
+
+int main(void)
+{
+    ios::sync_with_stdio(false), cin.tie(NULL);
+    cin >> buf;
+    cout << buf << "\?\?!\n";
+    return 0;
+}
