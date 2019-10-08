ed1a8056d77a26c2cdf7bc517c9970db44bf5f29
jazz4rabbit <jazz4rabbit@gmail.com>
2019-10-09 00:23:02 +0900

    :sparkles: BOJ/11654; 아스키 코드 (http://noj.am/11654)

---
+++ BOJ/11654/main.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false), cin.tie(NULL);
+    char ch;
+    cin >> ch;
+    cout << static_cast<int>(ch) << '\n';
+    return 0;
+}
