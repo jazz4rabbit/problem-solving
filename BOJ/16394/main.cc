b89187431847939d3d6133fc0caaeba5e96d3ce4
jazz4rabbit <jazz4rabbit@gmail.com>
2019-10-09 02:40:35 +0900

    :sparkles: BOJ/16394; 홍익대학교 (http://noj.am/16394)

---
+++ BOJ/16394/main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int N;
+
+int main(void)
+{
+    ios::sync_with_stdio(false), cin.tie(NULL);
+    cin >> N;
+    cout << N - 1946 << '\n';
+
+    return 0;
+}
