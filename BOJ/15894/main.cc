80cee2a75089fe795636fa8ec80a1f9583b1343b
jazz4rabbit <jazz4rabbit@gmail.com>
2019-10-09 02:33:00 +0900

    :sparkles: BOJ/15894; 수학은 체육과목 입니다 (http://noj.am/15894)

---
+++ BOJ/15894/main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+unsigned int n;
+
+int main(void)
+{
+    ios::sync_with_stdio(false), cin.tie(NULL);
+    cin >> n;
+    cout << 4*n << '\n';
+    return 0;
+}
