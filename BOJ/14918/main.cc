4cc4c0eb195858107be15c3acce0e1530ea7d8a4
jazz4rabbit <jazz4rabbit@gmail.com>
2019-10-09 02:37:26 +0900

    :sparkles: BOJ/14918; 더하기 (http://noj.am/14918)

---
+++ BOJ/14918/main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int A, B;
+
+int main(void)
+{
+    ios::sync_with_stdio(false), cin.tie(NULL);
+    cin >> A >> B;
+    cout << A+B << '\n';
+
+    return 0;
+}
