9aacb872738e6153823777a06367242f7d34bedf
jazz4rabbit <jazz4rabbit@gmail.com>
2019-10-09 00:14:00 +0900

    :sparkles: BOJ/10172; 개 (http://noj.am/10172)

---
+++ BOJ/10172/main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false), cin.tie(NULL);
+    cout << "|\\_/|\n" <<
+        "|q p|   /}\n" <<
+        "( 0 )\"\"\"\\\n" << 
+        "|\"^\"`    |\n" << 
+        "||_/=\\\\__|\n";
+    return 0;
+}
