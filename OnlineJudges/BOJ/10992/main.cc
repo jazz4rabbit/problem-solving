aff4c23b7410e86b47729c581f5afb8d4ca486ac
jazz4rabbit <jazz4rabbit@gmail.com>
2019-10-27 02:04:21 +0900

    :sparkles: BOJ/10992; 별 찍기 - 17 (http://noj.am/10992)

---
+++ OnlineJudges/BOJ/10992/main.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false), cin.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    for (int i=0; i<N-1; ++i) cout << ' ';
+    cout << "*\n";
+    for (int i=1; i<N-1; ++i) {
+        for (int j=0; j<N-i-1; ++j) cout << ' ';
+        cout << '*';
+        for (int j=0; j<2*i-1; ++j) cout << ' ';
+        cout << "*\n";
+    }
+    if (N != 1) {
+        for (int i=0; i<N*2-1; ++i) cout << '*';
+        cout << '\n';
+    }
+
+    return 0;
+}
