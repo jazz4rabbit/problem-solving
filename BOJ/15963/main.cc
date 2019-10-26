d3c906661357b62211502d8f9b618c3f3f7d0c95
jazz4rabbit <jazz4rabbit@gmail.com>
2019-10-27 01:33:55 +0900

    :sparkles: BOJ/15963; CASIO (http://noj.am/15963)

---
+++ BOJ/15963/main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false), cin.tie(NULL);
+
+    long long N, M;
+    cin >> N >> M;
+    cout << (N == M) << endl;
+
+    return 0;
+}
