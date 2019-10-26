93194d91c2f12b385487029c4cf3e9facc1bdcb4
jazz4rabbit <jazz4rabbit@gmail.com>
2019-10-27 02:02:16 +0900

    :sparkles: BOJ/10950; A+B - 3 (http://noj.am/10950)

---
+++ OnlineJudges/BOJ/10950/main.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false), cin.tie(NULL);
+    int T;
+    cin >> T;
+    while (T--) {
+        int A, B;
+        cin >> A >> B;
+        cout << A + B << '\n';
+    }
+
+    return 0;
+}
