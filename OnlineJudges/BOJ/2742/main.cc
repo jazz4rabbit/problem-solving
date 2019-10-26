d70a778d2be8740ab15af7d2fe71cf8966756191
jazz4rabbit <jazz4rabbit@gmail.com>
2019-10-27 02:27:12 +0900

    :sparkles: BOJ/2742; 기찍 N (http://noj.am/2742)

---
+++ OnlineJudges/BOJ/2742/main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false), cin.tie(NULL);
+    int N;
+    cin >> N;
+
+    do { cout << N << '\n'; } while (--N);
+
+    return 0;
+}
