cc203c506abc869583acc4a3fabd459b30eb7497
jazz4rabbit <jazz4rabbit@gmail.com>
2019-10-27 01:32:47 +0900

    :sparkles: BOJ/15680; 연세대학교 (http://noj.am/15680)

---
+++ BOJ/15680/main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false), cin.tie(NULL);
+
+    int N;
+    cin >> N;
+    cout << (N == 0 ? "YONSEI" : "Leading the Way to the Future") << endl;
+
+    return 0;
+}
