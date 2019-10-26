19eee0ed73cbad329c9fde2b56edc3c3b644ff0c
jazz4rabbit <jazz4rabbit@gmail.com>
2019-10-27 02:31:41 +0900

    :sparkles: BOJ/2959; 거북이 (http://noj.am/2959)

---
+++ OnlineJudges/BOJ/2959/main.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false), cin.tie(NULL);
+
+    int arr[4];
+    for (int i=0; i<4; ++i) cin >> arr[i];
+    sort(arr, arr+4);
+    cout << arr[0]*arr[2] << endl;
+
+    return 0;
+}
