69e210c4f36dcdcb4c50772e783caf789ebfff5f
jazz4rabbit <jazz4rabbit@gmail.com>
2019-10-27 01:31:47 +0900

    :sparkles: BOJ/15610; Abbey Courtyard (http://noj.am/15610)

---
+++ BOJ/15610/main.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false), cin.tie(NULL);
+
+    long long a;
+    cin >> a;
+    cout << fixed << setprecision(6) << std::sqrt(a)*4 << endl;
+    return 0;
+}
